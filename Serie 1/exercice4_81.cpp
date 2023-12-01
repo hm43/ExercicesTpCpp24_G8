acd079b05b9becc812b7492e40080bfe98986fca
Houda Mouttalib <houda.mouttalib@gmail.com>
2023-12-01 10:22:44 +0000

    solution exercice4 G81

--- Serie 1/exercice4_81.cpp
+++ Serie 1/exercice4_81.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 using namespace std;
+
 class Camion{
 private:
     string idc;
@@ -31,43 +32,23 @@ public:
         this->km = km;
         this->statut = statut;
     }
-    string getIdc() const{
+    string getIdc(){
         return idc;
     }
     void setIdc(string idc){
         this->idc = idc;
     }
-    string getModele() const{
-        return modele;
-    }
-    void setModele(string modele){
-        this->modele = modele;
-    }
-    double getCapacite() const{
-        return capacite;
-    }
-    void setCapacite(double capacite){
-        this->capacite = capacite;
-    }
-    double getKm() const{
-        return km;
-    }
-    void setKm(double km){
-        this->km = km;
-    }
-    string getStatut() const {
+    string getStatut(){
         return statut;
     }
     void setStatut(string statut){
         this->statut = statut;
     }
-
     void afficher(){
-        cout<<endl<<"L'id du camion: "<<idc<<endl;
-        cout<<"Le modele: "<<modele<<endl;
-        cout<<"La capacite: "<<capacite<<endl;
-        cout<<"kilometrage: "<<km<<endl;
-        cout<<"Le statut: "<<statut<<endl<<endl;
+        cout<<"L'id du camion: "<<idc<<endl;
+        cout<<"Le modele du camion: "<<modele<<endl;
+        cout<<"Le statut du camion: "<<statut<<endl;
+        
     }
 };
 class GestionnaireDeFlotte{
@@ -75,12 +56,8 @@ private:
     string nom;
     vector<Camion> flotte;
 public:
-    GestionnaireDeFlotte(){
-        nom = "";
-    }
-    GestionnaireDeFlotte(string nom){
-        this->nom = nom;
-    }
+    GestionnaireDeFlotte():nom(""){}
+    GestionnaireDeFlotte(string nom):nom(nom){}
     void ajouterCamion(){
         string idc;
         string modele;
@@ -89,17 +66,16 @@ public:
         string statut;
         
         int choix;
-        cout<<endl<<"  Menu Ajouter:"<<endl;
-        cout<<"1. Aucune information."<<endl;
-        cout<<"2. Id et Modele."<<endl;
-        cout<<"3. Toutes les informations."<<endl;
+        cout<<"1. Aucune information"<<endl;
+        cout<<"2. L'id et modele"<<endl;
+        cout<<"3. Toutes les informations"<<endl;
         cin>>choix;
+        
         switch (choix) {
             case 1:
                 {
                     Camion c;
                     flotte.push_back(c);
-                    cout<<"Ajoutee avec succee"<<endl;
                     break;
                 }
             case 2:
@@ -111,7 +87,6 @@ public:
                     getline(cin, modele);
                     Camion c2(idc, modele);
                     flotte.push_back(c2);
-                    cout<<"Ajoutee avec succee"<<endl;
                     break;
                 }
             case 3:
@@ -128,23 +103,25 @@ public:
                     cout<<"Donner le statut: "<<endl;
                     cin.ignore();
                     statut = Statut();
+                    
                     Camion c3(idc, modele, capacite, km, statut);
                     flotte.push_back(c3);
-                    cout<<"Ajoutee avec succee"<<endl;
                     break;
                 }
         }
     }
-    void majCamion(){
-        string idc, statut;
+    
+    void majStatut(){
+        string idc;
         cout<<"Donner l'id: ";
         cin.ignore();
         getline(cin, idc);
         
-        for(Camion &c: flotte){
+        for(Camion &c:flotte){
             if(c.getIdc().compare(idc) == 0){
                 string statut;
                 cout<<"Donner le statut: "<<endl;
+                
                 statut = Statut();
                 c.setStatut(statut);
                 break;
@@ -152,63 +129,83 @@ public:
         }
     }
     void afficher(){
-        cout<<"Le nom: "<<nom;
+        cout<<"Nom: "<<nom<<endl;
         for(Camion &c: flotte){
             c.afficher();
         }
     }
-    string Statut(){
-        string s[3] = {"En transit", "En chargement", "En maintenance"};
-        int choix;
-        cout<<"1. "<<s[0]<<endl;
-        cout<<"2. "<<s[1]<<endl;
-        cout<<"3. "<<s[2]<<endl;
+    void afficherParStatut(){
         
-        cin>>choix;
-        switch (choix) {
-            case 1:
-                return s[0];
-                break;
-            case 2:
-                return s[1];
-            case 3:
-                return s[2];
-            default:
-                cout<<"Invalide"<<endl;
-                return "";
-                break;
+        string statut = Statut();
+        cout<<"La liste des camions ayant le statut: "<<statut<<endl;
+        for(Camion &c: flotte){
+            if(c.getStatut().compare(statut) == 0){
+                c.afficher();
+            }
         }
     }
-    
+    string Statut(){
+        repeter:
+        cout<<"Liste des statut: "<<endl;
+        string s[3] = {"En transit", "En chargement", "En maintenance"};
+        for(int i = 0; i< 3 ; i++){
+            cout<<i+1<<". "<<s[i]<<endl;
+        }
+        int choix;
+            cin>>choix;
+            if(choix == 1){
+                return s[0];
+            }else if(choix == 2){
+                return s[1];
+            }else if(choix == 3){
+                return s[2];
+            }else{
+                cout<<"Invalide."<<endl;
+                goto repeter;
+            }
+    }
+    void menu(){
+        int choix;
+        do{
+            cout<<"      Menu      "<<endl;
+            cout<<"1. Ajouter un camion."<<endl;
+            cout<<"2. Modifier un camion."<<endl;
+            cout<<"3. Afficher le flotte."<<endl;
+            cout<<"4. Afficher le flotte selon statut."<<endl;
+            cout<<"5. Quitter."<<endl;
+            cout<<">> ";
+            cin>>choix;
+            switch (choix) {
+                case 1:
+                    ajouterCamion();
+                    break;
+                case 2:
+                    majStatut();
+                    break;
+                case 3:
+                    afficher();
+                    break;
+                case 4:
+                    afficherParStatut();
+                    break;
+                case 5:
+                    cout<<"EXIT."<<endl;
+                    break;
+                
+                default:
+                    cout<<"Mauvais choix."<<endl;
+                    break;
+            }
+        }while(choix != 5);
+    }
 };
 
-int main() {
+int main()
+{
+    
     GestionnaireDeFlotte g("X");
-    int choix;
-    do{
-        cout<<endl<<"      Menu"<<endl<<endl;
-        cout<<"1. Ajouter"<<endl;
-        cout<<"2. Modifier statut d'un camion"<<endl;
-        cout<<"3. Afficher les camions"<<endl;
-        cout<<"4. Quitter"<<endl;
-        cin>>choix;
-        switch (choix) {
-            case 1:
-                g.ajouterCamion();
-                break;
-            case 2:
-                g.majCamion();
-                break;
-            case 3:
-                g.afficher();
-                break;
-            case 4:
-                cout<<"EXIT."<<endl;
-                break;
-            default:
-                cout<<"Choix invalide."<<endl;
-                break;
-        }
-    }while(choix != 4);
+    g.menu();
+    
+    
     return 0;
 }
